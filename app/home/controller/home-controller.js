404cad73345f9b0575dd4f4a4601de1fde0d693e
sarwin-github <dapito.sherwin@yahoo.com>
2019-03-19 13:03:11 +0800

    added faqs and credits and blogs

--- app/home/controller/home-controller.js
+++ app/home/controller/home-controller.js
@@ -6,3 +6,19 @@ const async = require("async");
 module.exports.getHome = (req, res) => {
 	res.render('home/home.ejs', { success: true });
 };
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This will make a get request and render the index page for logging in a professional or client
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+module.exports.getBlogs = (req, res) => {
+	res.render('blog/blog.ejs', { success: true });
+};
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This will make a get request and render the index page for logging in a professional or client
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+module.exports.getCredits = (req, res) => {
+	res.render('faqs-credits/credits.ejs', { success: true });
+};
--- app/home/route/home-route.js
+++ app/home/route/home-route.js
@@ -6,4 +6,7 @@ const homeController = require('../controller/home-controller');
 /* Get The home page with list of fitness option */
 router.route('/').get(homeController.getHome);
 
+router.route('/our-blog').get(homeController.getBlogs);
+router.route('/faqs-credits').get(homeController.getCredits);
+
 module.exports = router;
