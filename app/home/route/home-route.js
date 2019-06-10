ba8fd8c4197ebcc08c139e7a7be76fc9253390ae
sarwin-github <dapito.sherwin@yahoo.com>
2019-06-10 10:56:31 +0800

    remove comment

--- app/home/route/home-route.js
+++ app/home/route/home-route.js
@@ -3,7 +3,6 @@ const router  = express();
 
 const homeController = require('../controller/home-controller');
 
-/* Get The home page with list of fitness option */
 router.route('/').get(homeController.getHome);
 
 router.route('/our-blog').get(homeController.getBlogs);
